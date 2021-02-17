8544f4beddc3b107b71afa2f99e776bc2f34dc0f
Vithya Nagamuthu <v_nagam@live.concordia.ca>
2021-02-17 15:00:34 -0500

    changes to driver

--- EmpireLegends/BiddingFacilityDriver.cpp
+++ EmpireLegends/BiddingFacilityDriver.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include "Player.h"
 
 using namespace std;
 
@@ -7,32 +9,33 @@ int main() {
 
 	int numofPlayers;
 	int PlayerCoins;
+	vector <Player> myPlayers; 
 
 	// determines how many coins per player
 	cout << "\n Number of players in the game (2-4): ";
 	cin >> numofPlayers;
 
-	while (numofPlayers > 4 || numofPlayers < 2) {
-		cout << "Invalid number of players";
-		cout << "\nNumber of players in the game (2-4): ";
-		cin >> numofPlayers;
-	}
-
-	if (numofPlayers == 2) {
+	switch (numofPlayers) {
+	case 2:
 		PlayerCoins = 14;
 		cout << " Each player gets 14 coins" << endl;
-
-	}
-	else if (numofPlayers == 3) {
+		break;
+	case 3:
 		PlayerCoins = 11;
 		cout << " Each player gets 11 coins" << endl;
-
-	}
-	else if (numofPlayers == 4) {
+		break;
+	case 4:
 		PlayerCoins = 9;
 		cout << " Each player gets 9 coins" << endl;
+		break;
+	default:
+		while (numofPlayers > 4 || numofPlayers < 2) {
+			cout << "Invalid number of players";
+			cout << "\nNumber of players in the game (2-4): ";
+			cin >> numofPlayers;
+		}
 	}
-
+	
 	// enable the player objects 
 
 
