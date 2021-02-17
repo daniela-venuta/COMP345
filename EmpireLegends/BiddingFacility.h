092ff5954082e7861470f7515ff7358d61da9ad2
Vithya Nagamuthu <v_nagam@live.concordia.ca>
2021-02-17 14:55:45 -0500

    changes to header

--- EmpireLegends/BiddingFacility.h
+++ EmpireLegends/BiddingFacility.h
@@ -1,1 +1,29 @@
 #pragma once
+#include <string>
+using namespace std;
+
+//class player;
+
+class BiddingFacility {
+
+	// Constructor 
+	BiddingFacility(string name, int bid, int coins);
+
+	// destructor 
+	~BiddingFacility();
+
+	// copy constructor
+	BiddingFacility(const BiddingFacility& obj);
+
+	// Assignment operator
+	BiddingFacility& operator=(const BiddingFacility& rhs);
+
+	// stream insertion operator 
+	friend ostream& operator<<(ostream& output, const BiddingFacility& BD);
+
+	// other methods 
+	int getBid();
+	void setBid();
+	int compareBid();
+
+};
