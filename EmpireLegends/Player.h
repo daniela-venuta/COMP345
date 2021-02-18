33422da60cfb12338642bebd3cc90804a67fc05d
emilie-martin <emmy.mar@hotmail.com>
2021-02-18 14:19:09 -0500

    added missing constructors and stream insertion operator overloading

--- EmpireLegends/Player.h
+++ EmpireLegends/Player.h
@@ -13,10 +13,25 @@ class Player
 {
 public:
 	Player(string username);
-	~Player();
+	~Player() = default;
+
+	// copy constructor
+	Player(const Player& otherPlayer);
+
+	// stream insertion operator overload
+	friend ostream& operator<<(ostream& os, const Player& player);
+	friend ostream& operator>>(ostream& os, const Player& player);
+
+	// assignment operator
+	Player& operator = (Player& player);
+
+	string getName();
+	void setName(string name);
 
 	void PayCoin(int price);
 	int getBalance();
+	void setBalance(int newBalance);
+
 	int MoveOverLand(Territory<Region>* from, Territory<Region>* to, GameMap* map);
 	void PlaceNewArmies(int number, Territory<Region>* destination);
 	void MoveArmies(int number, Territory<Region>* from, Territory<Region>* to, GameMap* map);
