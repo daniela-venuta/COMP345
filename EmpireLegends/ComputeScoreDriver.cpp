3ddb17d50b848be0e197e16b3ab5a4c6828e2b60
emilie-martin <emmy.mar@hotmail.com>
2021-03-28 22:21:31 -0400

    implement chooseWinner in MainGame

--- EmpireLegends/ComputeScoreDriver.cpp
+++ EmpireLegends/ComputeScoreDriver.cpp
@@ -1,117 +1,94 @@
+#include "MapLoader.h"
 #include "Player.h"
-
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include "Cards.h"
+#include "MainGame.h"
 
 int main() {
+
+	MapLoader* mapLoader = new MapLoader();
+	auto* gameMap = mapLoader->load("valid_map_1.json");
+
+	auto* r1 = gameMap->findTerritory("Continent 1")->value->findTerritory("Region 1");
+	auto* r2 = gameMap->findTerritory("Continent 1")->value->findTerritory("Region 2");
+	auto* r3 = gameMap->findTerritory("Continent 1")->value->findTerritory("Region 3");
+	auto* r4 = gameMap->findTerritory("Continent 2")->value->findTerritory("Region 4");
+
 	vector<Player*> players{};
-	players.push_back(new Player("p1"));
-	players.push_back(new Player("p2"));
+	Player* p1 = new Player("p1", 14);
+	players.push_back(p1);
+	Player* p2 = new Player("p2", 14);
+	players.push_back(p2);
 
-	// Compute individual VPs
-	for (Player* player : players) {
-		player->computeScore();
-	}
+	vector<Card*> cardVector;
 
-	std::cout << "Determining the winner...";
+	Card* card1 = new Card("Dire Dragon", new Flying, new Action("Place armies", 3) , new Action("Destroy armies", 3), AndOr::AND);
+	Card* card2 = new Card("Dire Giant", new Immune, new Action("Place armies", 3), new Action("Destroy armies", 3), AndOr::AND);
+	Card* card3 = new Card("Dire Eye", new Flying, new Action("Place armies", 4));
+	Card* card4 = new Card("Dire Ogre", new CoinVPs, new Action("Move armies", 2));
+	Card* card5 = new Card("Lake", new SetNameVPs(CardSet::forest), new Action("Place armies", 2), new Action("Move armies", 3),AndOr::AND);
+	Card* card6 = new Card("Noble Hills", new CompleteSetVPs(3, CardSet::noble), new Action("Place armies", 3));
+	Card* card7 = new Card("Arcane Phoenix", new Flying, new Action("Move armies", 5));
+	Card* card8 = new Card("Cursed Gargoyles", new Flying, new Action("Move armies", 5));
+	Card* card9 = new Card("Arcane Sphinx", new Flying, new Action("Place armies", 2), new Action("Move armies", 4), AndOr::OR);
+	Card* card10 = new Card("Dire Goblin", new Elixir(1), new Action("Move armies", 5));
+	Card* card11 = new Card("Forest Elf", new ExtraArmy, new Action("Place armies", 3), new Action("Move armies", 2), AndOr::OR);
+	Card* card12 = new Card("Forest Tree Town", new ExtraMove, new Action("Build city", 1));
+	Card* card13 = new Card("Noble Knight", new ExtraMove, new Action("Place armies", 4), new Action("Destroy armies", 1), AndOr::AND);
+	Card* card14 = new Card("Noble Unicorn", new ExtraMove, new Action("Move armies", 4), new Action("Place armies", 1), AndOr::AND);
+	Card* card15 = new Card("Night Hydra", new ExtraArmy, new Action("Move armies", 5), new Action("Destroy armies", 1), AndOr::AND);
+	Card* card16 = new Card("Night Village", new ExtraArmy, new Action("Build city", 1));
+	Card* card17 = new Card("Castle 1", new Elixir(1), new Action("Place armies", 3),new Action("Build city", 1), AndOr::OR);
+	Card* card18 = new Card("Forest Pixie", new ExtraArmy, new Action("Move armies", 4));
+	Card* card19 = new Card("Arcane Manticore", new ExtraMove, new Action("Place armies", 4), new Action("Destroy armies", 1), AndOr::AND);
+	Card* card20 = new Card("Ancient Woods", new ExtraArmy, new Action("Build city", 1), new Action("Place armies", 1), AndOr::AND);
+	Card* card21 = new Card("Forest Gnome", new Elixir(3), new Action("Move armies", 2));
+	Card* card22 = new Card("Castle 2", new Elixir(1), new Action("Move armies", 3), new Action("Build city", 1), AndOr::OR);
+	Card* card23 = new Card("Ancient Tree Spirit", new Elixir(3), new Action("Place armies", 4), new Action("Build city", 1), AndOr::OR);
+	Card* card24 = new Card("Ancient Woods", new ExtraArmy, new Action("Build city", 1), new Action("Place armies", 1), AndOr::AND);
+	Card* card25 = new Card("Cursed Banshee", new Elixir(2), new Action("Move armies", 5));
+	Card* card26 = new Card("Cursed King", new Elixir(1), new Action("Move armies", 4), new Action("Place armies", 3), AndOr::OR);
+	Card* card27 = new Card("Cursed Mausoleum", new ExtraMove, new Action("Build city", 1));
+	Card* card28 = new Card("Night Hydra", new ExtraArmy, new Action("Move 5 armies ", 5), new Action("Destroy armies", 1), AndOr::AND);
 
-	// 2VP for max elixir
-	Player* mostElixirs{};
-	for (int i = 0; i < players.size() - 1; i++) {
-		if (players.at(i)->getResources()->elixir > players.at(i+1)->getResources()->elixir) {
-			mostElixirs = players.at(i);
-		}
-		else {
-			mostElixirs = players.at(i+1);
-		}
-	}
+	cardVector.push_back(card1);
+	cardVector.push_back(card2);
+	cardVector.push_back(card3);
+	cardVector.push_back(card4);
+	cardVector.push_back(card5);
+	cardVector.push_back(card6);
+	cardVector.push_back(card7);
+	cardVector.push_back(card8);
+	cardVector.push_back(card9);
+	cardVector.push_back(card10);
+	cardVector.push_back(card11);
+	cardVector.push_back(card12);
+	cardVector.push_back(card13);
+	cardVector.push_back(card14);
+	cardVector.push_back(card15);
+	cardVector.push_back(card16);
+	cardVector.push_back(card17);
+	cardVector.push_back(card18);
+	cardVector.push_back(card19);
+	cardVector.push_back(card20);
+	cardVector.push_back(card21);
+	cardVector.push_back(card22);
+	cardVector.push_back(card23);
+	cardVector.push_back(card24);
+	cardVector.push_back(card25);
+	cardVector.push_back(card26);
+	cardVector.push_back(card27);
 
-	mostElixirs->addVictoryPoints(2);
+	Deck* deck = new Deck(cardVector);
 
-	// Determine winner
-	vector<Player*> highestVPplayers;
-	int highestVP = 0;
-	for (Player* player : players) {
-		int playerVP = player->getVictoryPoints();
-		if (playerVP <= highestVP) {
-			// New highest VP count
-			if (highestVP < playerVP)
-			{
-				highestVP = playerVP;
-				highestVPplayers.clear(); // Clearing highest VP player vector for new highest
-			}
-			highestVPplayers.push_back(player);
-		}
-	}
+	MainGame* mainGame = new MainGame(nullptr, nullptr, deck, players);
 
-	// Tie on most VPs (most coins owned wins)
-	vector<Player*> richestPlayers;
-	int highestCoins = 0;
+	p1->applyGood(card3->getGood());
+	p1->applyGood(card10->getGood());
+	p2->applyGood(card4->getGood());
+	p2->applyGood(card7->getGood());
+	p2->applyGood(card9->getGood());
 
-	if (highestVPplayers.size() > 1) {
-		std::cout << "\nThere is a tie for victory points." << std::endl;
-		std::cout << "\nSorting by number of coins..." << std::endl;
-
-		for (Player* player : players) {
-			int playerCoins = player->getCoins();
-			if (playerCoins <= highestCoins) {
-				// New highest coin count
-				if (highestCoins < playerCoins)
-				{
-					highestCoins = playerCoins;
-					richestPlayers.clear(); // Clearing highest coin count vector for new highest
-				}
-				richestPlayers.push_back(player);
-			}
-		}
-
-		// Tie on most coins (most armies owned wins)
-		vector<Player*> mostArmiesPlayers;
-		int highestArmyCount = 0;
-
-		if (richestPlayers.size() > 1) {
-			std::cout << "\nThere is a tie for number of coins." << std::endl;
-			std::cout << "\nSorting by number of armies..." << std::endl;
-
-			for (Player* player : players) {
-				int playerArmies = 18 - player->getResources()->unplacedArmies;
-				if (playerArmies <= highestArmyCount) {
-					// New highest army count
-					if (highestArmyCount < playerArmies)
-					{
-						highestArmyCount = playerArmies;
-						mostArmiesPlayers.clear(); // Clearing highest army count vector for new highest
-					}
-					mostArmiesPlayers.push_back(player);
-				}
-			}
-
-			// Tie on most armies (most regions owned wins)
-			Player* mostRegionsOwnedPlayer;
-			int highestNumberOfRegions = 0;
-
-			if (mostArmiesPlayers.size() > 1) {
-				std::cout << "\nThere is a tie for number of armies." << std::endl;
-				std::cout << "\nSorting by number of region..." << std::endl;
-
-				for (Player* player : players) {
-					int playerRegions = player->getTerritories().size();
-					if (highestNumberOfRegions < playerRegions) {
-						highestNumberOfRegions = playerRegions;
-						mostRegionsOwnedPlayer = player;
-					}
-				}
-			}
-			else {
-				std::cout << "The winner is " << mostRegionsOwnedPlayer->getName() << "!" << std::endl;
-			}
-		}
-		else {
-			std::cout << "The winner is " << mostArmiesPlayers.back() << "!" << std::endl;
-		}
-	}
-	else {
-		std::cout << "The winner is " << richestPlayers.back() << "!" << std::endl;
-	}
+	mainGame->chooseWinner();
+	std::cout << "P1 VP: " << p1->getVictoryPoints() << std::endl;
+	std::cout << "P2 VP: " << p2->getVictoryPoints() << std::endl;
 }
--- EmpireLegends/GameStartDriver.cpp
+++ EmpireLegends/GameStartDriver.cpp
@@ -1,49 +1,49 @@
-#include <iostream>
-#include <sstream>
-
-#include "GameStart.h"
-#include "Cards.h"
-
-using namespace std;
-
-int main() {
-
-	GameStart* startGame = new GameStart();
-
-	startGame->loadMap();
-	startGame->detPlayerCount();
-	startGame->playerBidding();
-
-	// CARD VARIABLES
-	Card* card1 = new Card("Dire dragon", new Flying, "Place 3 armies and destroy one army");
-	Card* card2 = new Card("Dire giant", new Immune, "Place 3 armies and destroy army");
-	Card* card3 = new Card("Dire eye", new Flying, "Place 4 armies");
-	Card* card4 = new Card("Dire Goblin", new Elixir(1), "Move 5 armies");
-	Card* card5 = new Card("Dire Ogre", new CoinVPs, "Move 2 armies");
-	Card* card6 = new Card("Lake", new SetNameVPs(CardSet::forest), "Place 2 armies and move 3 armies");
-	Card* card7 = new Card("Forest Elf", new ExtraArmy, "Place 3 armies or movie 2 armies");
-	Card* card8 = new Card("Forest Gnome", new Elixir(3), "Move 2 armies");
-	Card* card9 = new Card("Forest Tree Town", new ExtraMove, "Place a city");
-	Card* card10 = new Card("Graveyard", new SetNameVPs(CardSet::cursed), "Place 2 armies");
-	Card* card11 = new Card("Noble Hills", new CompleteSetVPs(3, CardSet::noble), "Place 3 armies");
-	Card* card12 = new Card("Castle", new Elixir(1), "Place 3 armies or build a city");
-
-	// Add Cards to Deck
-	vector<Card*> cardVector;
-	cardVector.push_back(card1);
-	cardVector.push_back(card2);
-	cardVector.push_back(card3);
-	cardVector.push_back(card4);
-	cardVector.push_back(card5);
-	cardVector.push_back(card6);
-	cardVector.push_back(card7);
-	cardVector.push_back(card8);
-	cardVector.push_back(card9);
-	cardVector.push_back(card10);
-	cardVector.push_back(card11);
-	cardVector.push_back(card12);
-
-	std::cout << "Printing deck:" << std::endl;
-	Deck* deck = new Deck(cardVector);
-	std::cout << *deck;
-}
+//#include <iostream>
+//#include <sstream>
+//
+//#include "GameStart.h"
+//#include "Cards.h"
+//
+//using namespace std;
+//
+//int main() {
+//
+//	GameStart* startGame = new GameStart();
+//
+//	startGame->loadMap();
+//	startGame->detPlayerCount();
+//	startGame->playerBidding();
+//
+//	// CARD VARIABLES
+//	Card* card1 = new Card("Dire dragon", new Flying, new Action("Place 3 armies", 3), new Action("Destroy one army", 1), AndOr::AND);
+//	Card* card2 = new Card("Dire giant", new Immune, new Action("Place 3 armies", 3), new Action("Destroy 1 army", 1), AndOr::AND);
+//	Card* card3 = new Card("Dire eye", new Flying, new Action("Place 4 armies", 4));
+//	Card* card4 = new Card("Dire Goblin", new Elixir(1), new Action("Move 5 armies", 5));
+//	Card* card5 = new Card("Dire Ogre", new CoinVPs, new Action("Move 2 armies", 2));
+//	Card* card6 = new Card("Lake", new SetNameVPs(CardSet::forest), new Action("Place 2 armies", 2), new Action("Move 3 armies", 3), AndOr::AND);
+//	Card* card7 = new Card("Forest Elf", new ExtraArmy, new Action("Place 3 armies", 3), new Action("Move 2 armies", 2), AndOr::OR);
+//	Card* card8 = new Card("Forest Gnome", new Elixir(3), new Action("Move 2 armies", 2));
+//	Card* card9 = new Card("Forest Tree Town", new ExtraMove, new Action("Place a city", 1));
+//	Card* card10 = new Card("Graveyard", new SetNameVPs(CardSet::cursed), new Action("Place 2 armies", 2));
+//	Card* card11 = new Card("Noble Hills", new CompleteSetVPs(3, CardSet::noble), new Action("Place 3 armies", 3));
+//	Card* card12 = new Card("Castle", new Elixir(1), new Action("Place 3 armies", 3), new Action("Build a city", 1), AndOr::OR);
+//
+//	// Add Cards to Deck
+//	vector<Card*> cardVector;
+//	cardVector.push_back(card1);
+//	cardVector.push_back(card2);
+//	cardVector.push_back(card3);
+//	cardVector.push_back(card4);
+//	cardVector.push_back(card5);
+//	cardVector.push_back(card6);
+//	cardVector.push_back(card7);
+//	cardVector.push_back(card8);
+//	cardVector.push_back(card9);
+//	cardVector.push_back(card10);
+//	cardVector.push_back(card11);
+//	cardVector.push_back(card12);
+//
+//	std::cout << "Printing deck:" << std::endl;
+//	Deck* deck = new Deck(cardVector);
+//	std::cout << *deck;
+//}
