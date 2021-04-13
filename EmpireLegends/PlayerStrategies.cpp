c545f03cf273e986b0ad88839351b419fcf322af
bakou912 <tom912_a_@hotmail.com>
2021-04-13 16:27:07 -0400

    implementing execution method for non humans

--- EmpireLegends/PlayerStrategies.cpp
+++ EmpireLegends/PlayerStrategies.cpp
@@ -1,5 +1,6 @@
+#include <stdlib.h>
+#include <time.h>
 #include "PlayerStrategies.h"
-
 #include "MapUtility.h"
 
 Action* HumanStrategy::chooseAction(Action* action1, Action* action2)
@@ -94,6 +95,94 @@ void HumanStrategy::executeAction(Action* action, Player* player, GameMap* map)
 			const int numArmies = action->getMultiplier();
 			std::cout << "You may place " << numArmies << " armies. Please choose a region: \n";
 			Territory<Region>* destination = player->chooseTerritory(MapUtility::printTerritoriesWithMap(map));
+			actionDone = player->placeNewArmies(numArmies, destination, MapUtility::getStartingLocation(map));
+		}
+	}
+}
+
+void NonHumanStrategy::executeAction(Action* action, Player* player, GameMap* map)
+{
+	const std::size_t build = action->getName().find("Build");
+	const std::size_t destroy = action->getName().find("Destroy");
+	const std::size_t place = action->getName().find("Place");
+	const std::size_t move = action->getName().find("Move");
+	bool actionDone = false;
+	srand(time(nullptr));
+
+	while (!actionDone)
+	{
+		//Build City
+		if (build != std::string::npos)
+		{
+			std::map<int, Territory<Region>*> destinations = MapUtility::printTerritoriesWithArmies(map, player);
+
+			if (destinations.empty())
+			{
+				std::cout << "There is no region where to build a city." << std::endl;
+				return;
+			}
+
+			std::cout << player->getName() << " may build a city in one of the regions with at least 1 army. " << std::endl;
+			actionDone = player->buildCity(destinations.at(rand() % destinations.size())); // Selecting random destination
+		}
+		//Move Armies
+		else if (move != std::string::npos)
+		{
+			std::cout << player->getName() << " may move" << action->getMultiplier() << " armies." << std::endl;
+
+			std::map<int, Territory<Region>*> froms = MapUtility::printTerritoriesWithArmies(map, player);
+			Territory<Region>* from = froms.at(rand() % froms.size()); // Selecting random initial region
+
+			std::map<int, Territory<Region>*> tos = MapUtility::printTerritoriesWithArmies(map, player);
+			Territory<Region>* to = tos.at(rand() % tos.size()); // Selecting random destination;
+
+			if (froms.empty() || tos.empty())
+			{
+				std::cout << "Moving armies is currently impossible." << std::endl;
+				return;
+			}
+
+			while (to == from)
+			{
+				to = tos.at(rand() % tos.size());
+			}
+
+			actionDone = player->moveArmies(action->getMultiplier(), from, to, map);
+		}
+		//Destroy Armies
+		else if (destroy != std::string::npos)
+		{
+			const int numArmies = action->getMultiplier();
+			std::map<int, Territory<Region>*> locations = MapUtility::printTerritoriesWithEnemyArmies(map, player, numArmies);
+
+			if (locations.empty())
+			{
+				std::cout << "There is no region where " << numArmies << " armies can be destroyed." << std::endl;
+				return;
+			}
+
+			std::cout << player->getName() << " may destroy " << numArmies << " enemy armies. Choosing a region. \n";
+
+			Territory<Region>* location = locations.at(rand() % locations.size());
+			Player* enemyPlayer = player->chooseEnemy(location, numArmies);
+
+			actionDone = player->destroyArmy(numArmies, location, enemyPlayer);
+		}
+		//Place New Armies
+		else if (place != std::string::npos)
+		{
+			std::map<int, Territory<Region>*> destinations = MapUtility::printTerritoriesWithMap(map);
+
+			const int numArmies = action->getMultiplier();
+			std::cout << player->getName() << " may place " << numArmies << " armies. Choosing a region.\n";
+			Territory<Region>* destination;
+
+			// Selecting a destination until it's valid
+			do
+			{
+				destination = destinations.at(rand() % destinations.size());
+			} 			while (destination != MapUtility::getStartingLocation(map) && destination->getPlacedCities(player) == 0);
+
 			actionDone = player->placeNewArmies(numArmies, destination, MapUtility::getStartingLocation(map));
 		}
 	}
--- EmpireLegends/PlayerStrategies.h
+++ EmpireLegends/PlayerStrategies.h
@@ -2,14 +2,28 @@
 #include "Cards.h"
 #include "Map.h"
 
+// Player trategy interface
 class PlayerStrategy
 {
 public:
 	virtual ~PlayerStrategy() = default;
+	/// <summary>
+	/// Choosing between 2 actions to play.
+	/// </summary>
+	/// <param name="action1"></param>
+	/// <param name="action2"></param>
+	/// <returns>Chosen action</returns>
 	virtual Action* chooseAction(Action* action1, Action* action2) = 0;
+	/// <summary>
+	/// Execution of an action.
+	/// </summary>
+	/// <param name="action"></param>
+	/// <param name="player"></param>
+	/// <param name="map"></param>
 	virtual void executeAction(Action* action, Player* player, GameMap* map) = 0;
 };
 
+// Human strategy. Choice and execution are dictated by user input.
 class HumanStrategy: public PlayerStrategy
 {
 public:
@@ -17,17 +31,22 @@ public:
 	void executeAction(Action* action, Player* player, GameMap* map) override;
 };
 
-class GreedyStrategy : public PlayerStrategy
+// Non human strategy. Execution is the same for Greedy and Moderate, but the choice method changes.
+class NonHumanStrategy : public PlayerStrategy
 {
 public:
-	Action* chooseAction(Action* action1, Action* action2) override;
 	void executeAction(Action* action, Player* player, GameMap* map) override;
 };
 
-class ModerateStrategy : public PlayerStrategy
+class GreedyStrategy : public NonHumanStrategy
 {
 public:
 	Action* chooseAction(Action* action1, Action* action2) override;
-	void executeAction(Action* action, Player* player, GameMap* map) override;
+};
+
+class ModerateStrategy : public NonHumanStrategy
+{
+public:
+	Action* chooseAction(Action* action1, Action* action2) override;
 };
 
